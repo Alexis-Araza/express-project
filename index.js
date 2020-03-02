47e0c68b8e931199e50864dd6e7ee53faafbf370
Ubuntu <vagrant@ubuntu-xenial.localdomain>
2020-03-02 02:20:26 +0000

    if statements

--- index.js
+++ index.js
@@ -20,20 +20,51 @@ app.use('/bootstrap', express.static(path.join(__dirname, 'node_modules/bootstra
 app.use('/jquery', express.static(path.join(__dirname, 'node_modules/jquery/dist')));
 app.use('/popper', express.static(path.join(__dirname, 'node_modules/@popperjs/core/dist/umd')));
 app.use('/js', express.static(path.join(__dirname, 'public/js')));
+app.use('/css', express.static(path.join(__dirname, 'public/css')));
 
 
 // set the route for index.html
 app.get('/index', (req,res,)=>{
   res.sendFile(path.join(__dirname+'/public/index.html'));
 });
+
 // set the route for about.html
 app.get('/about', (req,res,)=>{
   res.sendFile(path.join(__dirname+'/public/about.html'));
 });
+
 //give access to apiData
 app.get('/practice', (req,res,)=>{
   res.json(apiData);
 });
 
+//set endpoint/=parameter
+app.get('/gender/g=:gender',(req,res)=>{
+	const genderParam = req.params.gender // retrieves the parameter value requested by user
+	if ((genderParam === 'male') || (genderParam === 'female')){
+		let filteredArray = []; // array to push the matching objects to user's value
+		for (let i = 0; i < apiData.length; i++) {
+			if (genderParam === apiData[i].gender.toLowerCase()){
+				filteredArray.push(apiData[i]);
+			}
+		}
+		res.send(filteredArray);
+	} else {
+		res.send('Invalid parameter');
+	}
+
+});
+
+//set name/=first_name
+app.get('/name/fn=:first_name',(req,res)=>{
+	const nameParam = req.params.first_name // retrieves the parameter value requested by user
+		let nameFilteredArray = []; // array to push the matching objects to user's value
+		for (let i = 0; i < apiData.length; i++) {
+			if (nameParam === apiData[i].first_name.toLowerCase()){
+				nameFilteredArray.push(apiData[i]);
+			}
+		}
+		res.send(nameFilteredArray);
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,8 @@
-console.log('Hello');
+console.log('check, check; is express.js working?');
+
+$(document).ready(function (){
+  $('h1').click(function(){
+    $(this).css('background','orange');
+  })
+
+});
